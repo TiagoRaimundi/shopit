9dee93820fa72ec50b711c7c194ac9e47917a6e1
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-04-18 19:01:13 -0300

    update product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,11 @@
 import express from "express"
-import { getProducts, getProductsDetails, newProduct } from "../controllers/productControllers.js"
+import { getProducts, getProductsDetails, newProduct, updateProduct } from "../controllers/productControllers.js"
 
 const router = express.Router()
 
 router.route("/products").get(getProducts);
 router.route("/admin/products").post(newProduct);
-
 router.route("/products/:id").get(getProductsDetails);
+router.route("/products/:id").patch(updateProduct);
 
 export default router;
