a90b64950db74f690c79233ccaabab57bba4bc1e
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-04-18 09:41:03 -0300

    packjson - DEV MODE & PROD MODE

--- backend/app.js
+++ backend/app.js
@@ -5,5 +5,7 @@ import dotenv from 'dotenv'
 dotenv.config({path: 'backend/config/config.env'})
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server started on PORT: ${process.env.PORT}`);
-})
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
+});
+
+
