3d13aea3802135e2878f0fb6fd02a7ab179132b4
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-04-19 08:47:09 -0300

    creating middlewareError

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 const app = express();
 import dotenv from 'dotenv'
 import { connectDatabase } from './config/dbConnect.js';
+import errorMiddleware from "./middleware/error.js"
 
 dotenv.config({path: 'backend/config/config.env'});
 
@@ -14,6 +15,10 @@ app.use(express.json());
 import productRoutes from "./routes/products.js"
 
 app.use("/api/v1", productRoutes)
+
+
+//Using error middleware
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`);
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,5 +1,6 @@
 import product from "../models/product.js";
 import Product from "../models/product.js";
+import ErrorHandler from "../utils/errorHandler.js";
 
 //Create new Product => /api/v1/products
 export const getProducts = async (req, res) => {
@@ -22,18 +23,17 @@ export const newProduct = async (req, res) => {
 
 
 //Get single Product details => /api/v1/products/:id
-export const getProductsDetails = async (req, res) => {
+export const getProductsDetails = async (req, res, next) => {
 
     const product = await Product.findById(req?.params?.id) 
     
     if(!product) {
-        return res.status(404).json({
-            error: 'Product not found',
-        })
+        return next(new ErrorHandler('Product not found', 404));
     }
 
     res.status(200).json({
-        product
+        product,
+        
     })
 }
 
