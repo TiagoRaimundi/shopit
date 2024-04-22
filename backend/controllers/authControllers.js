184760f623d5bcc28f474491f034ffcf141c9e35
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-04-22 11:28:23 -0300

    Encrypting Password while Registration

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,6 +1,14 @@
-import catchAsyncErrors from "../middleware/catchAsyncErrors";
+import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
+import User from "../models/user.js";
 
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
     const {name, email, password} = req.body;
-    
-})
+
+    const user = await User.create({
+        name, email, password
+    })
+
+    res.status(201).json({
+        success: true,
+    });
+});
