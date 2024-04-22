217566513e33d9e689653c0af0ce4ad64a08a378
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-04-22 13:45:21 -0300

    Generate JSON Web Token

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -6,9 +6,13 @@ export const registerUser = catchAsyncErrors(async (req, res, next) => {
 
     const user = await User.create({
         name, email, password
-    })
+    });
+
+
+    const token = user.getJwtToken();
+
 
     res.status(201).json({
-        success: true,
+        token,
     });
 });
