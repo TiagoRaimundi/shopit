da9008ad3f58792fac82ce5951275e82aa4e023b
TiagoRaimundi <raimunditiagoaugusto@gmail.com>
2024-05-08 17:12:14 -0300

    Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import { Footer } from "./components/layouts/Footer";
 import Header from "./components/layouts/Header";
 import "./App.css";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      
+      <div className="container">
+        <Home/>
+      </div>
       <Footer/>
     </div>
   );
